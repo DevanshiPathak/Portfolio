bc3eb66a0e85368eafdf38546571c0c4c2d9671f
DevanshiPathak <devanshijani07@gmail.com>
2022-07-05 15:12:07 +0530

    cv

---
+++ resume/src/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+
+    });
+
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
+    //toggle menu/ navbar script
+    $('.menubtn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menubtn i').toggleClass("active");
+    });
+    //typing animation script
+    var typed=new typed (".typing",{
+        strings:["Developer"],
+        typeSpeed: 100,
+        backSpeed:60,
+        loop:true
+    });
+
+});
